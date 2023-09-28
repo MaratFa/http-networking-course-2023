cfe0edd9ec9cbd7ef79ac911bf450ea125062ec1
Marat <evolution99@rambler.ru>
2023-09-29 00:20:30 +0300

    Exercise solutions

---
+++ 1 Course/3 URLs/7 The Protocol/index.js
@@ -0,0 +1,9 @@
+function getMailtoLinkForEmail(email) {
+  return `mailto:${email}`;
+}
+
+let email = "slayer@fquest.app";
+console.log(`The mailto link for ${email} is: ${getMailtoLinkForEmail(email)}`);
+
+email = "killer@fquest.app";
+console.log(`The mailto link for ${email} is: ${getMailtoLinkForEmail(email)}`);
